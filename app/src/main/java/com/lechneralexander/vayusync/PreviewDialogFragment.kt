cba7fe8e8249291d2b4e8b90095986fce09c4c61
Alexander Lechner <alexander.lechner@ingka.ikea.com>
2025-07-27 17:18:38 +0200

    main: add exif information

--- app/src/main/java/com/lechneralexander/vayusync/PreviewDialogFragment.kt
+++ app/src/main/java/com/lechneralexander/vayusync/PreviewDialogFragment.kt
@@ -2,6 +2,7 @@ package com.lechneralexander.vayusync
 
 import android.app.Dialog
 import android.graphics.drawable.Drawable
+import androidx.exifinterface.media.ExifInterface
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.MediaController
+import android.widget.TextView
 import android.widget.VideoView
 import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.DialogFragment
@@ -74,9 +76,52 @@ class PreviewDialogFragment : DialogFragment() {
             imageView.setOnViewTapListener { _, _, _ -> dismiss() }
         }
 
+        val exifView = view.findViewById<TextView>(R.id.fullExifInfo)
+        val exifInfo = loadExifInfo(uri) ?: "No EXIF information!"
+        exifView.text = exifInfo
+
         view.setOnClickListener { dismiss() }
 
         return view
+    }
+
+    private fun loadExifInfo(uri: Uri): String? {
+        requireContext().contentResolver.openInputStream(uri)?.use { inputStream ->
+            val exif = ExifInterface(inputStream)
+            val make = exif.getAttribute(ExifInterface.TAG_MAKE)
+            val model = exif.getAttribute(ExifInterface.TAG_MODEL)
+            val datetime = exif.getAttribute(ExifInterface.TAG_DATETIME)
+            val focalLength = exif.getAttribute(ExifInterface.TAG_FOCAL_LENGTH)
+            val iso = exif.getAttribute(ExifInterface.TAG_ISO_SPEED)
+            val exposureTime = exif.getAttribute(ExifInterface.TAG_EXPOSURE_TIME)
+            val width = exif.getAttributeInt(ExifInterface.TAG_IMAGE_WIDTH, -1)
+            val height = exif.getAttributeInt(ExifInterface.TAG_IMAGE_LENGTH, -1)
+            val sizeBytes = context?.contentResolver?.openAssetFileDescriptor(uri, "r")?.use { afd ->
+                afd.length
+            } ?: -1L
+            val lensMake = exif.getAttribute(ExifInterface.TAG_LENS_MAKE)
+            val aperture = exif.getAttribute(ExifInterface.TAG_F_NUMBER)
+            val shutterSpeed = exif.getAttribute(ExifInterface.TAG_EXPOSURE_TIME)
+
+            val exifInfo = buildString {
+                listOfNotNull(
+                    if (make != null || model != null) "Camera: $make $model" else null,
+                    focalLength?.let { "Focal Length: $it" },
+                    iso?.let { "ISO: $it" },
+                    exposureTime?.let { "Exposure: $it s" },
+                    datetime?.let { "Taken: $it" },
+                    if (width > 0 && height > 0) "Resolution: ${width}x${height}" else null,
+                    if (sizeBytes > 0) "File size: ${sizeBytes / 1024} KB" else null,
+                    lensMake?.let { "Lens make: $it" },
+                    aperture?.let { "Aperture: f/$it" },
+                    shutterSpeed?.let { "Shutter speed: $it s" }
+                ).forEach { appendLine(it) }
+            }
+            if (exifInfo.isNotEmpty()) {
+                return exifInfo
+            }
+        }
+        return null
     }
 
     private fun getCachedPreview(uri: Uri, imageLoader: ImageLoader): Drawable? {
